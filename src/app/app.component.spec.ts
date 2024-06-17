09088b7bd281c005db1944f7cdf5a647c1e9f48c
udin jelek <udin@jelek.com>
2024-06-17 18:37:24 +0700

    update card-eid-2024 to cards

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'card-eid-2024'`, () => {
+  it(`should have as title 'cards'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('card-eid-2024');
+    expect(app.title).toEqual('cards');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('card-eid-2024 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cards app is running!');
   });
 });
